6f414b63c97096362fe4369fed78d396632f3d54
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2024-12-09 20:55:03 +0600

    Added level-0 logo

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <main className="flex text-6xl tracking-widest font-bold font-sans min-h-screen flex-col items-center justify-center p-24 text-white bg-slate-900 ">
+   <main className="flex text-6xl tracking-widest font-bold font-sans min-h-screen flex-col items-center justify-center text-white bg-slate-900 p-12">
     <div className="group relative">
       <div className="opacity-100 duration-300 ">
         N<span className="text-amber-500 italic">i</span>fad Uzzaman<div  className="bg-amber-500 h-2 w-2 rounded-sm inline-block"> </div>
       </div>
       <span className="text-xs  absolute -top-6 left-0 rounded-sm text-amber-500 py-1 px-3 tracking-normal  bg-slate-700  delay-200 duration-200 opacity-0 group-hover:opacity-100 ">Developer of this webpage.</span>
+  
     </div>
+    <div className="absolute bottom-0 left-0 text-sm  m-20 opacity-40 italic">
+        Level-0 
 
+      </div>
    </main>
   );
 }
